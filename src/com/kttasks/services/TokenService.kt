62cba0a2d1662c4719dc5af326c6389a28e2b772
Felipe Megale <felipe.megale17@gmail.com>
2020-12-31 14:47:05 -0300

    format document

--- src/com/kttasks/services/TokenService.kt
+++ src/com/kttasks/services/TokenService.kt
@@ -6,16 +6,24 @@ import com.auth0.jwt.algorithms.Algorithm
 import com.kttasks.dtos.UserDto
 import java.util.*
 
-class TokenService(private val issuer: String, private val audience: String, private val expirationTime: Long, private val jwtSecret: String) {
+class TokenService(
+    private val issuer: String,
+    private val audience: String,
+    private val expirationTime: Long,
+    private val jwtSecret: String
+) {
     private val algorithm = Algorithm.HMAC256(this.jwtSecret)
     private fun getExpiresAt() = Date(System.currentTimeMillis() + this.expirationTime)
 
-    fun buildJwtVerifier(): JWTVerifier = JWT.require(algorithm).withIssuer(this.issuer).build()
+    fun buildJwtVerifier(): JWTVerifier = JWT.require(algorithm)
+        .withIssuer(this.issuer)
+        .build()
 
     fun generateToken(userInfo: UserDto): String {
         return JWT.create()
             .withSubject("Authentication")
             .withIssuer(this.issuer)
+            .withAudience(this.audience)
             .withClaim("name", userInfo.username)
             .withClaim("password", userInfo.password)
             .withExpiresAt(getExpiresAt())
